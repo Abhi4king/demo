7d181d66a80966bad862bd0f15622ddb3085523e
Abhi4king <122840195+Abhi4king@users.noreply.github.com>
2025-12-03 08:05:47 +0530

    bbb

--- ver2-allbooks/vite.config.ts
+++ ver2-allbooks/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-        allowedHosts: ['https://demo-2vur.onrender.com'],
+        allowedHosts: ['demo-2vur.onrender.com'],
       },
       plugins: [react()],
       define: {
