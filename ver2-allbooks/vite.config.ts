1a4474e6a01cf7a65660f2c09569373bc18430b4
Abhi4king <122840195+Abhi4king@users.noreply.github.com>
2025-12-03 07:58:57 +0530

    Update vite.config.ts

--- ver2-allbooks/vite.config.ts
+++ ver2-allbooks/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        Allowedhost: 'https://demo-2vur.onrender.com',
       },
       plugins: [react()],
       define: {
